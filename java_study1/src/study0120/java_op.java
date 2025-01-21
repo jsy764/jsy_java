cfa6a99fbc0752495785d319dcc0254fb59b49e1
jsy <aa_nen@naver.com>
2025-01-21 09:16:28 +0900

    save

--- java_study1/src/study0120/java_op.java
+++ java_study1/src/study0120/java_op.java
@@ -32,6 +32,18 @@ public class java_op {
 		 * 사과 상자는 11개, 오렌지 상자는 12개 있다면
 		 * 과일은 총 몇개인가?
 		 * (출력결과는 친절하게~)
+		 * 
+		 * int apple = 20;
+		 * int appleBox = 11;
+		 * int orange = 15;
+		 * int orangeBox = 12;
+		 * 
+		 * int totalApple = appleBox * apple;
+		 * int totalOrange = orangeBox * orange;
+		 * int totalfruit = totalApple + totalOrange;
+		 * System.out.println("사과 총 갯수 : " + totalApple);
+		 * System.out.println("오렌지 총 갯수 : " + totalOrange);
+		 * System.out.println("과일 총 갯수 : " + totalFruit);
 		 */
 	}
 }
