079b04823ff0beaa8c58704739a259cb41f0525e
jsy <aa_nen@naver.com>
2025-01-24 12:48:02 +0900

    save

--- java_study1/src/study0123/JavaBitOp.java
+++ java_study1/src/study0123/JavaBitOp.java
@@ -7,6 +7,10 @@ public class JavaBitOp {
 		System.out.println(27 & 13); // and 연산
 		System.out.println(27 | 13); // or 연산
 		System.out.println(27 ^ 13); // xor연산
+		System.out.println( ~13 ); // not 연산
+		
+		//비트 쉬프트 연산자
+		System.out.println( 21<<2 ); // << 시프트 연산자
+		System.out.println( 17>>3 ); // >> 시프트 연산자
 	}
-
 }
