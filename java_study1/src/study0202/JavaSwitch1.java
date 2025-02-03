78a651ec922a4aa99f0be9609aece57c435e4747
jsy <aa_nen@naver.com>
2025-02-04 08:42:31 +0900

    save

--- java_study1/src/study0202/JavaSwitch1.java
+++ java_study1/src/study0202/JavaSwitch1.java
@@ -1,5 +1,7 @@
 package study0202;
 
+import java.util.Scanner;
+
 public class JavaSwitch1 {
 	public static void main(String[] args) {
 //		switch(20) {
@@ -23,39 +25,67 @@ public class JavaSwitch1 {
 //			break;
 //		}
 		
-		int num = 50;
-		switch(num) {
-		case 10:
-			System.out.println("10 선택");
-			break;
-		case 30:
-			System.out.println("30 할꺼야??");
-			break;
-		case 20:
-			System.out.println("case에 표현하는 데이터는 정수, 문자, 문자열");
-			break;
-		case 50:
-			System.out.println("입력된 값에 맞는 case 실행은 "+"break 또는 } 까지 실행");
-			break;
-		default:
-			System.out.println("case에 없는값이야!! 다시해!!");
+//		int num = 50;
+//		switch(num) {
+//		case 10:
+//			System.out.println("10 선택");
+//			break;
+//		case 30:
+//			System.out.println("30 할꺼야??");
+//			break;
+//		case 20:
+//			System.out.println("case에 표현하는 데이터는 정수, 문자, 문자열");
+//			break;
+//		case 50:
+//			System.out.println("입력된 값에 맞는 case 실행은 "+"break 또는 } 까지 실행");
+//			break;
+//		default:
+//			System.out.println("case에 없는값이야!! 다시해!!");
+//		}
+		
+//		int 점수=72;
+//		switch(점수/10) {
+//		case 10:
+//		case 9:	
+//			System.out.println("A 학점");
+//			break;
+//		case 8:
+//			System.out.println("B 학점");
+//			break;
+//		case 7:
+//			System.out.println("C 학점");
+//			break;
+//		default:
+//			System.out.println("F 학점");
+//		}
+		
+		Scanner sc = new Scanner(System.in);
+		int user1 = (int)(Math.random()*3+1);
+		System.out.println("1.가위 2.바위 3.보");
+		System.out.print("입력 : ");
+		int user2 = sc.nextInt();
+		if(user1 == user2) // 둘이 같을 경우
+			System.out.println("비겼다.");
+		else if(user1 == 1) { // 둘이 다를 경우
+			if(user2 == 2)
+				System.out.println("user2의 승리");
+			else // user2 == 3
+				System.out.println("user1의 승리");
+		}
+		else if(user1 == 2) {
+			if(user2 == 1)
+				System.out.println("user1의 승리");
+			else // user2 == 3
+				System.out.println("user2의 승리");
+		}
+		else if(user1 == 3) {
+			if(user2 == 1) 
+				System.out.println("user2의 승리");
+			else // user2 == 2
+				System.out.println("user1의 승리");
 		}
 		
-		int 점수=72;
-		switch(점수/10) {
-		case 10:
-		case 9:	
-			System.out.println("A 학점");
-			break;
-		case 8:
-			System.out.println("B 학점");
-			break;
-		case 7:
-			System.out.println("C 학점");
-			break;
-		default:
-			System.out.println("F 학점");
-		}
+		
 		
 	}
 }
