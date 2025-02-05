4c6dc3b735abf5d146c3de12aeb8a8f643e82709
jsy <aa_nen@naver.com>
2025-02-05 11:53:38 +0900

    save

--- java_study1/src/study0225/JavaLoop2.java
+++ java_study1/src/study0225/JavaLoop2.java
@@ -9,13 +9,24 @@ public class JavaLoop2 {
 	
 	public static void main(String[] args) {
 		
-		int num=0;
-		for(;;) {
-			System.out.print("정수 입력 : ");
-			num=sc.nextInt();
-		}
+//		for(int i=1; i<=2; i++) {
+//			
+//			System.out.println("변수 i 사용 반복문 "+i);
+//			for(int k=1; k <=3; k++) {
+//				
+//				System.out.println(i+"	변수 k 사용  "+k);
+//			}
+//		}
 		
-		
+//		int num=0;
+//		for(;num!=100;) {
+//			System.out.print("정수 입력 : ");
+//			num=sc.nextInt();
+//		}
+//		// for( ;; ), for(; true;), for(; num!=0;)
+//		while(true) {
+//			
+//		}
 		
 		
 //		for( ; ; ) { // for문 무한 루프
@@ -46,6 +57,22 @@ public class JavaLoop2 {
 //		}
 //		System.out.println("10부터 100까지의 총합: "+sum);
 		
+//		for(int i=2; i<=9; i++) {
+//			for(int j=1; j<=9; j++) {
+//				System.out.printf("%d*%d=%d\n", i, j, i*j);
+//				// System.out.println(k+"*"+i+"="+(k*i));
+//			}
+//		}
+		
+//		int i=1;
+//		for(int k=2; k<=9;) {
+//			System.out.println(k+"*"+i+"="+(k*i));
+//			i++;
+//			if(i==10) {
+//				k++;
+//				i=1;
+//			}
+//		}
 		
 	}
 }
---
+++ java_study1/src/study0225/JavaLoop3.java
@@ -0,0 +1,21 @@
+package study0225;
+
+public class JavaLoop3 {
+
+	public static void main(String[] args) {
+		
+		//for(초기값;조건식;증감식)
+		//for(int i=1; i<=10; i++)
+		// 반복횟수가 정해져 있는 경우 for문이 가독성이 좋다.
+		// 반복횟수가 없는 경우 while문이 가독성이 좋다.
+		int i=1; // 초기값
+		
+		while(i<=10) { //조건식
+			System.out.println(i);
+			i++; //증감식
+		}
+		
+		
+		
+	}
+}
