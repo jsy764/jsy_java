61fa5f2737c6bd1fce11a112f9aca8ddd9f78d3b
jsy <aa_nen@naver.com>
2025-02-05 12:06:56 +0900

    save

--- java_study1/src/study0205/JavaLoop3.java
+++ java_study1/src/study0205/JavaLoop3.java
@@ -11,11 +11,15 @@ public class JavaLoop3 {
 		//for(int i=1; i<=10; i++)
 		// 반복횟수가 정해져 있는 경우 for문이 가독성이 좋다.
 		// 반복횟수가 없는 경우 while문이 가독성이 좋다.
-		int i=1; // 초기값
-		
-		while(i<=10) { //조건식
-			System.out.println(i);
-			i++; //증감식
+//		int i=1; // 초기값
+//		
+//		while(i<=10) { //조건식
+//			System.out.println(i);
+//			i++; //증감식
+//		}
+		while(true) {
+			System.out.print("정수 입력 : ");
+			int num = scan.nextInt();
 		}
 		
 		
