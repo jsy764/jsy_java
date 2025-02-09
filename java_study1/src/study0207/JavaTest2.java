c994746cd22d2b81156c3be4b4a76d636f694007
jsy <aa_nen@naver.com>
2025-02-10 08:54:52 +0900

    save

--- java_study1/src/study0207/JavaTest2.java
+++ java_study1/src/study0207/JavaTest2.java
@@ -1,8 +1,11 @@
 package study0207;
 
+import java.util.Scanner;
+
 public class JavaTest2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		/*
 		 문제 4
 		 	*
@@ -45,27 +48,27 @@ public class JavaTest2 {
 		 	5. 비밀번호와 비밀번호 역순의 숫자를 더하기 하면 16566이다. 예 1234 + 4321
 		 	6. 위 조건을 모두 충족하는 비밀번호 출력
 		 */
-		int a, b, c, d;
-		for(a = 1; a <= 9; a++) {
-			for(b = 1; b <= 9; b++) {
-				for(c = 1; c<=9; c++) {
-					for(d = 1; d<=9; d++) {
-						if(a!=b && a!=c && a!=d && b!=c & b!=d && c!=d) {
-							if(a == d+2) {
-								if(b < c) {
-									int pw = a*1000+b*100+c*10+d;
-									int rPw = d*1000+c*100+b*10+a;
-									if((pw+rPw) ==16566) {
-										System.out.println(pw);
-										break;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+//		int a, b, c, d;
+//		for(a = 1; a <= 9; a++) {
+//			for(b = 1; b <= 9; b++) {
+//				for(c = 1; c<=9; c++) {
+//					for(d = 1; d<=9; d++) {
+//						if(a!=b && a!=c && a!=d && b!=c & b!=d && c!=d) {
+//							if(a == d+2) {
+//								if(b < c) {
+//									int pw = a*1000+b*100+c*10+d;
+//									int rPw = d*1000+c*100+b*10+a;
+//									if((pw+rPw) ==16566) {
+//										System.out.println(pw);
+//										break;
+//									}
+//								}
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
 		// 문제 7. 알파벳 찾기(반드시 반복문 사용해서)
 		//	알파벳(대문자 또는 소문자 모두 가능하게) 입력
 		// 입력한 알파벳이 몇번째 알파벳인지 찾기 - 반드시 for문으로
@@ -82,5 +85,99 @@ public class JavaTest2 {
 		// 결과 : 0스트라이크 1볼 2아웃
 		// 입력 : 4 1 8
 		// 결과 : 2스트라이크 0볼 1아웃
+		int a, b, c, num1, num2, num3;
+		while(true) {
+			a = (int)(Math.random()*9+1);
+			b = (int)(Math.random()*9+1);
+			c = (int)(Math.random()*9+1);
+			if(a!=b && a!=c && b!=c)
+				break;
+		}
+		while(true) {
+			System.out.print("num1 숫자입력: ");
+			num1 = sc.nextInt();
+			System.out.print("num2 숫자입력: ");
+			num2 = sc.nextInt();
+			System.out.print("num3 숫자입력: ");
+			num3 = sc.nextInt();
+			if(a==num1&&b==num2&&c==num3) {
+				System.out.println("3스트라이크 0볼 0아웃");
+				break;
+			} else if((a==num1&&b==num2)||(b==num2&&c==num3)||(a==num1&&c==num3)) { 
+				System.out.println("2스트라이크 0볼 1아웃");
+			} else if(a==num1) {
+				if(b==num3 && c==num2) 
+						System.out.println("1스트라이크 2볼 0아웃");
+				else if(b==num3 || c==num2)
+					System.out.println("1스트라이크 1볼 1아웃");
+				else 
+					System.out.println("1스트라이크 0볼 2아웃");
+			}else if(b==num2) {
+				if(a==num3 && c==num1) 
+					System.out.println("1스트라이크 2볼 0아웃");
+				else if(a==num3 || c==num1)
+					System.out.println("1스트라이크 1볼 1아웃");
+				else 
+					System.out.println("1스트라이크 0볼 2아웃");
+			}else if(c==num3) {
+				if(a==num2 && b==num1) 
+					System.out.println("1스트라이크 2볼 0아웃");
+				else if(a==num2 || b==num1)
+					System.out.println("1스트라이크 1볼 1아웃");
+				else 
+					System.out.println("1스트라이크 0볼 2아웃");
+			}else if(a!=num1&&b!=num2&&c!=num3) {
+				if(a==num2) {
+					if(b==num3&&c==num1)
+						System.out.println("0스트라이크 3볼 0아웃");
+					else if(b==num3||c==num1)
+						System.out.println("0스트라이크 2볼 1아웃");
+					else
+						System.out.println("0스트라이크 1볼 2아웃");
+				}
+				else if(a==num3) {
+					if(b==num1&&c==num2)
+						System.out.println("0스트라이크 3볼 0아웃");
+					else if(b==num1||c==num2)
+						System.out.println("0스트라이크 2볼 1아웃");
+					else
+						System.out.println("0스트라이크 1볼 2아웃");
+				}
+				else if(b==num1) {
+					if(a==num3&&c==num2)
+						System.out.println("0스트라이크 3볼 0아웃");
+					else if(a==num3||c==num2)
+						System.out.println("0스트라이크 2볼 1아웃");
+					else
+						System.out.println("0스트라이크 1볼 2아웃");
+				}
+				else if(b==num3) {
+					if(a==num2&&c==num1)
+						System.out.println("0스트라이크 3볼 0아웃");
+					else if(a==num2||c==num1)
+						System.out.println("0스트라이크 2볼 1아웃");
+					else
+						System.out.println("0스트라이크 1볼 2아웃");
+				}
+				if(c==num1) {
+					if(a==num2&&b==num3)
+						System.out.println("0스트라이크 3볼 0아웃");
+					else if(a==num2||b==num3)
+						System.out.println("0스트라이크 2볼 1아웃");
+					else
+						System.out.println("0스트라이크 1볼 2아웃");
+				}
+				else if(c==num2) {
+					if(a==num3&&b==num1)
+						System.out.println("0스트라이크 3볼 0아웃");
+					else if(a==num3||b==num1)
+						System.out.println("0스트라이크 2볼 1아웃");
+					else
+						System.out.println("0스트라이크 1볼 2아웃");
+				}
+				else if(a!=num1 && a!=num2 && a!=num3 && b!=num1 && b!=num2 && b!=num3 && c!=num1 && c!=num2 && c!=num3)
+					System.out.println("0스트라이크 0볼 3아웃");	
+			}
+		}
 	}
 }
