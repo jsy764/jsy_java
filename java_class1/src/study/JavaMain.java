c5c157b942f3bd70c5f9172013f3ef151ff55438
jsy <aa_nen@naver.com>
2025-02-18 10:01:07 +0900

    save

--- java_class1/src/study/JavaMain.java
+++ java_class1/src/study/JavaMain.java
@@ -3,16 +3,28 @@ package study;
 public class JavaMain {
 
 	public static void main(String[] args) {
-		// 
+		 
+		// 데이터 저장 - 변수
+		// 같은 타입의 많은 양의 데이터 - 배열
 		
-		A info;
+		// 이름, 나이, 직업, 몸무게, 시력, 생년월일
+		// 100명의 데이터를 저장하기 위해서는 서로 다른 종류의 데이터이기에 100의 크기인 1차원 배열 6개를 만들어야한다.
+		// String[] names = new String[100];
+		
+//		struct member{ // 구조체 = member 타입
+//			String name;
+//			int age;
+//		}
+//		member info; // member타입의 변수 info
+//		info.name="김유신";
+//		info.age=24;
+//		member[] mem = new member[100];
+		
+		
+		
+		
 	}
 
 }
 
 
-
-class A{
-	int num;
-	float f;
-}
