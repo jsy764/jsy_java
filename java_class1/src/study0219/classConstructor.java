27ee775dab0f211dd049c4e38a5433019b833422
jsy <aa_nen@naver.com>
2025-02-19 11:56:33 +0900

    save

--- java_class1/src/study0219/classConstructor.java
+++ java_class1/src/study0219/classConstructor.java
@@ -3,8 +3,14 @@ package study0219;
 public class classConstructor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Food f1 = new Food("돈가스", 8000);
+		
+		System.out.println(f1.foodName);
+		
+		Food f2 = new Food();
+// 기본생성자 메서드가 존재하기에 Food() 실행됨
+		
 	}
 
 }
