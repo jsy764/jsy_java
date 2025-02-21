14d0464aeaad550c41b979cf5ccee367b00eaa5d
jsy <aa_nen@naver.com>
2025-02-21 09:57:45 +0900

    save

---
+++ java_class1/src/study0221/Courses.java
@@ -0,0 +1,5 @@
+package study0221;
+
+public class Courses {
+
+}
---
+++ java_class1/src/study0221/JavaMethod3.java
@@ -0,0 +1,10 @@
+package study0221;
+
+public class JavaMethod3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
