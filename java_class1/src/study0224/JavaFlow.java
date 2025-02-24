c202d4cf156b3f05ddfdf92898da442e70b1ff8e
jsy <aa_nen@naver.com>
2025-02-24 12:44:10 +0900

    save

---
+++ java_class1/src/study0224/JavaFlow.java
@@ -0,0 +1,14 @@
+package study0224;
+
+public class JavaFlow {
+
+	static void total(Game g1, Game g2, Game g3) {
+		int tot = g1.cnt + g2.cnt+g3.cnt;
+		System.out.println("총합 : "+ tot);
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
