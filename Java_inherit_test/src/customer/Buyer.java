56cb87413ee1a4e4a58b0b8a11f184d7eadb3dce
jsy <aa_nen@naver.com>
2025-02-27 14:05:39 +0900

    save

--- Java_inherit_test/src/customer/Buyer.java
+++ Java_inherit_test/src/customer/Buyer.java
@@ -1,15 +1,20 @@
 package customer;
 
+import java.util.Date;
+
 import store.Fruit;
 
 public class Buyer {
 	private int money;
 	private Fruit item;// 구매한 음료 또는 컵과일 저장 변수
+	private Date buyDate;
 	
 	public Buyer(int money) { // Buyer 객체생성시 동작 할 생성자 메서드
 		this.money=money;
 	}
 
+	// set, get 메서드
+	
 	public int getMoney() {
 		return money;
 	}
@@ -26,11 +31,18 @@ public class Buyer {
 		this.item = item;
 	}
 	
-	// set, get 메서드
-	public String toString() { 
-		return "보유머니 : "+money;
+	public Date getBuyDate() {
+		return buyDate;
 	}
 
+	public void setBuyDate(Date buyDate) {
+		this.buyDate = buyDate;
+	}
+
+	public String toString() { 
+		return "보유머니 : "+money+"원";
+	}
+	
 	
 	
 }
