acd8e1701b5dd187ef9194214149e513a5babf4e
jsy <aa_nen@naver.com>
2025-02-28 17:30:21 +0900

    save

--- Java_innerClass/src/inner1/MainMt.java
+++ Java_innerClass/src/inner1/MainMt.java
@@ -3,8 +3,22 @@ package inner1;
 public class MainMt {
 
 	public static void main(String[] args) {
+//		Test.A a = new Test.A();
+		Test t = new Test();
 		
-
+		t.setNum(10); // 내부클래스가 노출 안됨
+		t.innerView();
+		
+		t.a.num = 10; // 내부클래스가 노출됨
+		
+//		Test.A a = t.new A();
+//		a.num=10;
+//		a.num=20;
+		
+		// 정적 내부클래스를 굳이 노출 하면서 객체를 만들고자 한다면
+		Test.Dog dog = new Test.Dog();
+		
+		Test.origin(); // 클래스 메서드실행은 객체없이 가능하다.
 	}
 
 }
--- Java_innerClass/src/inner1/Test.java
+++ Java_innerClass/src/inner1/Test.java
@@ -4,13 +4,44 @@ public class Test {
 
 	static int count=1; // 클래스변수 (정적변수)
 	int age; // 인스턴스변수
+	A a;
 	
+	Test(){
+		a=new A();
+	}
+	static void origin() { // 클래스 메서드
+
+	// 정적 내부 클래스
+	}
+	// 정적 내부 클래스(static)
+	static class Dog{
+		int year;
+	}
+	void setYear(int year) {
+		Dog d = new Dog();
+		d.year=year;
+	}
+	
+	// 인스턴스 내부클래스 정의 - 외부클래스의 객체생성해야 사용
+	class A{
+		int num;
+		void view() {System.out.println("내부클래스 메서드");}
+	}
+	
+	void innerView() {
+		this.a.view();
+	}
+	
+	void setNum(int num) {
+		this.a.num=num;
+	}
 	void sum() {
 		int a, b; // 지역변수
 	}
 }
 
 // 내부클래스 - 클래스 내부에 만드는 클래스
+// 내부클래스는 여러개의 클래스와 관계를 맺는게 아니라 하나의 클래스와 상속 또는 포함 관계를 맺을 경우에 사용하면 효과적이다.
 
 /*
 	static 변수 또는 메서드
