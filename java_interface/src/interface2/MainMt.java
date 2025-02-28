7154121ba6cfc269364e7f5795220ae7111f09cd
jsy <aa_nen@naver.com>
2025-02-28 11:37:00 +0900

    save

---
+++ java_interface/src/interface2/MainMt.java
@@ -0,0 +1,13 @@
+package interface2;
+
+public class MainMt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		store 떡 = new 떡볶이();
+		떡.cooking();
+		store 카 = new 카페();
+		카.cooking();
+	}
+
+}
