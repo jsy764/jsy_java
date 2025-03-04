0a7c7a25587ce355fa2b9fa9eb599eb587babf06
jsy <aa_nen@naver.com>
2025-03-04 17:12:41 +0900

    save

--- Java_Exception/src/exception4/Member.java
+++ Java_Exception/src/exception4/Member.java
@@ -9,4 +9,27 @@ public class Member {
 		this.name = name;
 		this.age = age;
 	}
+	
+	public void nameCount() {
+		
+		if( name == null) {
+			throw new NullPointerException("이름 입력안함");
+			// 예외 발생 시키기 throw
+		}
+		if(name.length()==2) {
+			System.out.println("이름이 외자이다.");
+		} else if( name.length()==4) {
+			System.out.println("이름이 3자이다.");
+		} else if( name.length()==3) {
+			System.out.println("이름이 2자이다.");
+		}
+	}
+	
+	public int getBirthYear() throws Exception {
+		if(age == 0) { // 나이 입력 안한 경우
+			throw new Exception("나이 입력안함");
+		}
+		return 2025-age;
+	}
+	
 }
