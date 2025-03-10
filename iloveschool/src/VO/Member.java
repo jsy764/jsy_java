6c9136c9dd906f6605cd7bc97449bd45f1b0c433
jsy <aa_nen@naver.com>
2025-03-10 17:35:22 +0900

    save

--- iloveschool/src/VO/Member.java
+++ iloveschool/src/VO/Member.java
@@ -1,5 +1,21 @@
 package VO;
 
-public class Member {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Member {
+	private Long id;
+	private String memberId;
+	private String email;
+	private String password;
+	private String birth; 
 }
--- iloveschool/src/VO/School.java
+++ iloveschool/src/VO/School.java
@@ -1,5 +1,26 @@
 package VO;
 
-public class School {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class School {
+	private int id;
+	private String diff;
+	private String schoolName;
+	private String address;
+	private int postCode;
+	private String tel;
+	private String fax;
+	private String education_office;
+	private String public_private;
+	private String hompage;
+	
 }
