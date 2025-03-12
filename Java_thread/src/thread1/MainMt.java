4857d8bb5495f8c6e3ad57ae09c8ac3f106556cd
jsy <aa_nen@naver.com>
2025-03-12 17:30:02 +0900

    save

--- Java_thread/src/thread1/MainMt.java
+++ Java_thread/src/thread1/MainMt.java
@@ -1,20 +1,42 @@
 package thread1;
 
+import java.util.Scanner;
+
 public class MainMt {
 
 	public static void main(String[] args) {
 		
-		MyThread th1 = new MyThread("장은호");
-		th1.start();
+//		MyThread th1 = new MyThread("장은호");
+//		th1.start();
+//		
+//		MyThread th2 = new MyThread("김명준");
+//		th2.start();
+//		
+//		for(int i=1; i<=10; i++) {
+//			System.out.println(i+"main메서드");
+//			try {
+//				Thread.sleep(1200);
+//			}catch(Exception e) {}
+//		}
 		
-		MyThread th2 = new MyThread("김명준");
-		th2.start();
+		Dino[] 알 = new Dino[] {
+				new Dino("티라노사우루스", 10),
+				new Dino("밸로시랩터", 4),
+				new Dino("프테라노돈", 6),
+				new Dino("스테고사우루스", 7)
+		};
+		Create device1 = new Create();
+		device1.start();
+		// 부화 시킬 공룡을 선택하여 부화 진행 할것이다.
+		// 스레드를 통해 각 공룔별 시간만큼 동작하도록 할것이다.
+		Scanner sc = new Scanner(System.in);
 		
-		for(int i=1; i<=10; i++) {
-			System.out.println(i+"main메서드");
-			try {
-				Thread.sleep(1200);
-			}catch(Exception e) {}
+		for(int i=0; i<알.length; i++) {
+			System.out.print("1~4 중 하나 : ");
+			int select = sc.nextInt();
+			
+			Create.hatch(알[select-1]);
+			
 		}
 	}
 
